714547d6f9379f5977e9adcc1b8c6427a7934393
rcjoestar <rcjoestar@gmail.com>
2025-11-24 00:25:44 +0100

    Add Hardware and Composants tables to database

--- base.sql
+++ base.sql
@@ -1,1 +1,17 @@
+CREATE TABLE Hardware (
+    hardware_id INT PRIMARY KEY,
+    nom VARCHAR(50),
+    type VARCHAR(20),
+    annee YEAR,
+    details TEXT
+);
 
+CREATE TABLE Composants (
+    composant_id INT PRIMARY KEY,
+    nom VARCHAR(50),
+    type VARCHAR(20),
+    annee YEAR,
+    details TEXT,
+    hardware_id INT,
+    FOREIGN KEY (hardware_id) REFERENCES Hardware(hardware_id)
+);
