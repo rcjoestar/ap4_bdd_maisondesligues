ee3d28db8abe91d14706649a4f490eb3946f2ee7
rcjoestar <rcjoestar@gmail.com>
2026-02-17 09:51:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 
 <?php 
+
+require_once 'credentials.php';
+
+
 $user = 'root';
 $password = 'root';
 
-
-
+try
+{
 $connexion = new PDO ('mysql:host=localhost;dbname=M2L2', $user, $password);
-
+}  catch (PDOException $e)
+{ print "Erreur :" . $e->getMessage() . "<br/>";
+  die;
+}
 ?>
